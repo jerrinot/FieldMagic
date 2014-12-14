9c870b4492bc1d240a183d490b12eee92119823c
Jaromir Hamala <jaromir.hamala@gmail.com>
2014-12-14 20:44:03 +0000

    Refactoring

--- src/main/java/uk/co/rockstable/experiements/codegen/reflection/extractors/impl/TypeDefinition.java
+++ src/main/java/uk/co/rockstable/experiements/codegen/reflection/extractors/impl/TypeDefinition.java
@@ -2,22 +2,22 @@ package uk.co.rockstable.experiements.codegen.reflection.extractors.impl;
 
 public enum TypeDefinition {
     OBJECT(false, null, null, null),
-    INT(true, "I", "java/lang/Integer", "int"),
-    BOOLEAN(true, "Z", "java/lang/Boolean", "boolean"),
-    CHAR(true, "C", "java/lang/Character", "char"),
-    BYTE(true, "B", "java/lang/Byte", "byte"),
-    SHORT(true, "S", "java/lang/Short", "short"),
-    LONG(true, "J", "java/lang/Long", "long"),
-    FLOAT(true, "F", "java/lang/Float", "float"),
-    DOUBLE(true, "D", "java/lang/Double", "double");
+    INT(true, "I", "java/lang/Integer", Integer.TYPE),
+    BOOLEAN(true, "Z", "java/lang/Boolean", Boolean.TYPE),
+    CHAR(true, "C", "java/lang/Character", Character.TYPE),
+    BYTE(true, "B", "java/lang/Byte", Byte.TYPE),
+    SHORT(true, "S", "java/lang/Short", Short.TYPE),
+    LONG(true, "J", "java/lang/Long", Long.TYPE),
+    FLOAT(true, "F", "java/lang/Float", Float.TYPE),
+    DOUBLE(true, "D", "java/lang/Double", Double.TYPE);
 
     private final boolean primitive;
-    private final String primitiveClassName;
+    private final Class<?> primitiveClassName;
     private final String jvmPrimitiveTypeCode;
     private final String boxingClass;
     private final String boxingMethodSignature;
 
-    private TypeDefinition(boolean primitive, String jvmPrimitiveTypeCode, String boxingClass, String primitiveClassName) {
+    private TypeDefinition(boolean primitive, String jvmPrimitiveTypeCode, String boxingClass, Class<?> primitiveClassName) {
         this.primitive = primitive;
         this.jvmPrimitiveTypeCode = jvmPrimitiveTypeCode;
         this.boxingClass = boxingClass;
@@ -50,10 +50,9 @@ public enum TypeDefinition {
     }
 
     public static TypeDefinition fromClass(Class<?> clazz) {
-        String className = clazz.getName();
         if (clazz.isPrimitive()) {
             for (TypeDefinition type : TypeDefinition.values()) {
-                if (className.equals(type.primitiveClassName)) {
+                if (clazz.equals(type.primitiveClassName)) {
                     return type;
                 }
             }
